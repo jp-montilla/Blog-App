0e5af073dbd8c234276216542140de7d739d2a39
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-07-06 20:24:49 +0800

    fresh

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(
+	name: "Admin",
+	email: "email@email.com",
+	role: "admin",
+	password: "password",
+	password_confirmation: "password"
+)

71d83b5839ff138d3eb94d51d84e65d91a338293
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-07-07 01:20:17 +0800

    Fixed comment error

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,8 +2,9 @@ class ProfileController < ApplicationController
 	def show
 		@profile = Admin.find(params[:id])
 	end
-	
+
 	def view
 		@post = Post.find(params[:id])
+		@comment = Comment.new
 	end
 end

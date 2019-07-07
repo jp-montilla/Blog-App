1ea9598ef2bf112062e209298c6c9c32b07263ac
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-07-07 19:24:37 +0800

    Updated Layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.order(updated: :desc)
     @add_post = Post.new
   end
 
